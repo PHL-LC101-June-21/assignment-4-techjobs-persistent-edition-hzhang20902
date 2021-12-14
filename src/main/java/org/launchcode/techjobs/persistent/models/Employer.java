1a878b792cfb4879378333fff69872038f02c2e6
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2021-12-14 00:18:57 -0500

    tasktwo passed

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @Size(min=3,max=55, message = "Location must be between 3 and 50 characters long")
+    @NotBlank
+    private static String location;
+
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(){}
+
+
 
 
 }
